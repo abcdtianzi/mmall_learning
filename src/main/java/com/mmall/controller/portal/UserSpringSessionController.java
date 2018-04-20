167884684a10e90b08ab67658300ffd153553e97
Ting <ting@ting-2.local>
2018-04-20 22:26:46 +0800

    spring session

---
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,74 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by ting on 2018/3/10.
+ * 学习spirng Session
+ */
+
+@Controller
+@RequestMapping("/user/springsession/")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService userService;
+
+
+    /**
+     * 用户登录
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        ServerResponse<User> response = userService.login(username, password);
+        //设置session
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+        }
+        return response;
+    }
+
+    /**
+     * 用户登出
+     *
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+
+        return ServerResponse.createBySuccess();
+    }
+
+
+    /**
+     * 获取session用户信息
+     *
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+    }
+
+
+}
